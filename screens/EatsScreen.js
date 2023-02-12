8f4cb76b06849d35a48b34a86509cb1678e8a715
Daniel Lee <daniel.lee@circleci.com>
2023-02-11 16:18:35 -0800

    screens to navigate

---
+++ screens/EatsScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const FoodScreen = () => {
+    return (
+        <View>
+            <Text>FOOOOOOOOOOOD!</Text>
+        </View>
+    );
+};
+
+export default FoodScreen;
+
+const styles = StyleSheet.create({});
