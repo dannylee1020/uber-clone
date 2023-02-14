890b5cddee146e4273f5e3d053080292ee1255b8
Daniel Lee <daniel.lee@circleci.com>
2023-02-13 21:15:46 -0800

    screen for displaying google map

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,14 +1,40 @@
-import { StyleSheet, Text, View } from "react-native";
+import { View } from "react-native";
 import React from "react";
+import tw from "twrnc";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import Map from "../components/Map";
+import NavigateCard from "../components/NavigateCard";
+import RideOptions from "../components/RideOptions";
 
 const MapScreen = () => {
+    const Stack = createNativeStackNavigator();
+
     return (
         <View>
-            <Text>MapScreen here!</Text>
+            <View style={tw`h-1/2`}>
+                <Map />
+            </View>
+            <View style={tw`h-1/2`}>
+                <Stack.Navigator>
+                    <Stack.Screen
+                        name="NavigateCard"
+                        component={NavigateCard}
+                        options={{
+                            headerShown: false,
+                        }}
+                    ></Stack.Screen>
+                    <Stack.Screen
+                        name="RideOptions"
+                        component={RideOptions}
+                        options={{
+                            headerShown: false,
+                        }}
+                    ></Stack.Screen>
+                </Stack.Navigator>
+            </View>
         </View>
     );
 };
 
 export default MapScreen;
-
-const styles = StyleSheet.create({});
