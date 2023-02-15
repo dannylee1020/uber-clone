d7d6a0ab810894fcf981fc67ba5a4a4dbc1a58e3
Daniel Lee <daniel.lee@circleci.com>
2023-02-14 21:28:10 -0800

    add keyboard avoiding view

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { Provider } from "react-redux";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { KeyboardAvoidingView } from "react-native";
 
 import { store } from "./store";
 import HomeScreen from "./screens/HomeScreen";
@@ -15,29 +16,35 @@ export default function App() {
         <Provider store={store}>
             <NavigationContainer>
                 <SafeAreaProvider>
-                    <Stack.Navigator>
-                        <Stack.Screen
-                            name="HomeScreen"
-                            component={HomeScreen}
-                            options={{
-                                headerShown: false,
-                            }}
-                        />
-                        <Stack.Screen
-                            name="MapScreen"
-                            component={MapScreen}
-                            options={{
-                                headerShown: false,
-                            }}
-                        />
-                        <Stack.Screen
-                            name="EatsScreen"
-                            component={EatsScreen}
-                            options={{
-                                headerShown: false,
-                            }}
-                        />
-                    </Stack.Navigator>
+                    <KeyboardAvoidingView
+                        style={{ flex: 1 }}
+                        behavior={Platform.OS === "ios" ? "padding" : "height"}
+                        keybordVerticalOffset={Platform.OS === "ios" ? -64 : 0}
+                    >
+                        <Stack.Navigator>
+                            <Stack.Screen
+                                name="HomeScreen"
+                                component={HomeScreen}
+                                options={{
+                                    headerShown: false,
+                                }}
+                            />
+                            <Stack.Screen
+                                name="MapScreen"
+                                component={MapScreen}
+                                options={{
+                                    headerShown: false,
+                                }}
+                            />
+                            <Stack.Screen
+                                name="EatsScreen"
+                                component={EatsScreen}
+                                options={{
+                                    headerShown: false,
+                                }}
+                            />
+                        </Stack.Navigator>
+                    </KeyboardAvoidingView>
                 </SafeAreaProvider>
             </NavigationContainer>
         </Provider>
